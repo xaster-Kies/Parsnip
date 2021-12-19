28040a41d55b5bf6c057bda0f6c96b648aeeef22
xaster-Kies <samueledward40@gmail.com>
2021-12-19 12:41:28 +0100

    taskList page

--- parsnip/src/components/Task.js
+++ parsnip/src/components/Task.js
@@ -6,6 +6,8 @@ const Task = props => {
             <div className="task-header">
                 <div>{props.task.title}</div>
             </div>
+            <hr />
+            <div className="task-body">{props.task.description}</div>
         </div>
     )
 }
---
+++ parsnip/src/components/TaskPage.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import TaskList from './TaskList';
+
+
+const TASK_STATUSES = ['Unstarted', 'In Progress', 'Completed'];
+
+export default class TaskPage extends Component {
+   
+    renderTaskLists() {
+       const { tasks } = this.props;
+       return TASK_STATUSES.map(status => {
+           const statusTasks = tasks.filter(task => task.status === status);
+           return <TaskList key={status} status={status} tasks = {statusTasks} />
+       })
+   }
+
+    
+    render() {
+        return (
+            <div className='tasks'>
+                <div className="task-lists">
+                    {this.renderTaskLists()}
+                </div>
+            </div>
+        )
+    }
+}
